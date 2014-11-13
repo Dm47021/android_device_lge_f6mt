47761a385c35e6fd0da8bcbfdf0353708fed1048
ccaapton <tr071@qq.com>
2014-11-13 04:52:47 -0600

    Add IR device property

--- f6mt.mk
+++ f6mt.mk
@@ -23,6 +23,7 @@ $(call inherit-product-if-exists, device/lge/f6mt/f6mt-vendor-blobs.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
